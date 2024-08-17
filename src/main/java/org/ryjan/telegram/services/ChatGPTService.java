c5a8c019377ee68edc4c1fee2bb8b2bccd04649b
Ryjan <ryjukyt@gmail.com>
2024-08-17 16:22:05 +0300

    ToManyRequests not working gpt

--- src/main/java/org/ryjan/telegram/services/ChatGPTService.java
+++ src/main/java/org/ryjan/telegram/services/ChatGPTService.java
@@ -1,6 +1,5 @@
 package org.ryjan.telegram.services;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.ryjan.telegram.config.BotConfig;
 
@@ -8,7 +7,6 @@ import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import javax.print.attribute.standard.JobKOctets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -30,11 +28,10 @@ public class ChatGPTService {
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set("Authorization", "Bearer " + API_KEY);
 
-        Map<String, Object> requestBody = new HashMap<>();
-        requestBody.put("model", "gpt-3.5-turbo");
-        List<Map<String, String>> messages = new ArrayList<>();
-        messages.add(Map.of("role", "user", "content", testQuestion));
-        requestBody.put("messages", messages);
+        Map<String, Object> requestBody = Map.of(
+                "model", "gpt-3.5-turbo",
+                "messages", List.of(Map.of("role", "user", "content", question))
+        );
         HttpEntity<Map<String, Object>> entity = new HttpEntity<>(requestBody, headers);
 
         ResponseEntity<String> response = restTemplate.exchange(API_URL, HttpMethod.POST, entity, String.class);
