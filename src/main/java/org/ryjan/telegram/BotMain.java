a7032c780f4eb3c6b4e40fc159f9c4d8ac3cec3b
Ryjan <ryjukyt@gmail.com>
2024-08-31 16:05:04 +0300

    Redesigned everything
    (cherry picked from commit f95c3dc98a70636541008f0e2391c849960fff93)

--- src/main/java/org/ryjan/telegram/BotMain.java
+++ src/main/java/org/ryjan/telegram/BotMain.java
@@ -6,21 +6,13 @@ import com.sun.tools.javac.Main;
 import org.ryjan.telegram.commands.user.UserService;
 import org.ryjan.telegram.handler.ButtonCommandHandler;
 import org.ryjan.telegram.config.BotConfig;
-<<<<<<< HEAD
-import org.ryjan.telegram.services.BotService;
-=======
 
 import org.ryjan.telegram.model.UserDatabase;
->>>>>>> 1497e62 (Spring boot is working!)
 import org.ryjan.telegram.utils.UpdateContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
-import org.springframework.beans.factory.annotation.Value;
-=======
->>>>>>> 1497e62 (Spring boot is working!)
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
@@ -34,15 +26,15 @@ import org.telegram.telegrambots.meta.api.objects.User;
 
 @SpringBootApplication
 public class BotMain extends TelegramLongPollingBot {
-    @Value("${bot.token}")
-    private String botToken;
-    @Value("${bot.username}")
-    private String botUsername;
+    @Autowired
+    private UserService userService;
+
+    private final ButtonCommandHandler buttonCommandHandler = new ButtonCommandHandler(this);
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     private static final String OWNER_ID = "2323";
 
-<<<<<<< HEAD
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-=======
     public static void main(String[] args) {
         SpringApplication.run(BotMain.class, args);
         LOGGER.info("Starting Bot...");
@@ -57,10 +49,11 @@ public class BotMain extends TelegramLongPollingBot {
            // userService.update(user);
            // System.out.println(userService.isOwner(user.getId()));
 
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private BotService botService;
+            LOGGER.info("Bot started successfully!");
+        } catch (TelegramApiException e) {
+            LOGGER.error("Error occurred while initializing Bot", e);
+        }
+    }
 
     public void sendMessage(String chatId, String text) {
         SendMessage message = new SendMessage();
@@ -84,12 +77,8 @@ public class BotMain extends TelegramLongPollingBot {
         }
 
         try {
-<<<<<<< HEAD
-            botService.handleUpdate(update);
-=======
             UserDatabase userDatabase = userService.findUser("Ryjan4ik");
             //buttonCommandHandler.handleCommand(update);
->>>>>>> 1497e62 (Spring boot is working!)
         } catch (Exception e) {
             LOGGER.error("Error occurred while sending message(onUpdateReceived)", e);
         }
@@ -97,15 +86,11 @@ public class BotMain extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
-        return botUsername;
+        return BotConfig.BOT_NAME;
     }
 
     @Override
     public String getBotToken() {
-        return botToken;
-    }
-
-    public Logger getLogger() {
-        return LOGGER;
+        return BotConfig.BOT_TOKEN;
     }
 }
